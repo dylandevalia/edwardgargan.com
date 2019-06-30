80964c133f1628d7afe73cea26af3144bdbd9f87
egargan <edwgargan@gmail.com>
2019-06-30 21:39:09 +0100

    Add 'copied to clipboard' popup for email

--- script/copy-email.js
+++ script/copy-email.js
@@ -1,3 +1,18 @@
-var emaillink = document.getElementById('email-link');
+// Initialise clipboard.js for email element
+const emailLink = document.getElementById('email-link');
+const clipboard = new ClipboardJS(emailLink);
 
-var clipboard = new ClipboardJS(emaillink);
+// Popup hide timeout holder
+var hidePopupTimeout;
+
+// onclick to be used by activating 'copy email' element
+function emailOnClick() {
+    clearTimeout(hidePopupTimeout);
+
+    var copiedPopup = document.getElementById('copied-popup');
+    copiedPopup.classList.add("show");
+
+    hidePopupTimeout = setTimeout(function() {
+        copiedPopup.classList.remove("show")
+    }, 1500);
+}
